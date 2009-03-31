da7cd0786531d91dce36bbc12af1068006554b54
Andy Mikhailenko <andy@neithere.net>
2009-04-01 00:43:06 +0600

    Add setuptools support (part 2 of 2)

--- autoslugfield/__init__.py
+++ autoslugfield/__init__.py
@@ -1,1 +1,6 @@
- 
+# -*- coding: utf-8 -*-
+
+__author__  = 'Andy Mikhailenko'
+__license__ = 'GNU Lesser General Public License (GPL), Version 3'
+__url__     = 'http://bitbucket.org/neithere/django-autoslugfield/'
+__version__ = '0.9'
