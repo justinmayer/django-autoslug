9d6b6c4b2f7f625f425777287f9031aa4e405322
Andrey Mikhaylenko <neithere@gmail.com>
2011-10-30 10:16:36 +0500

    Added coverage reports to the test runner.

--- run_tests.py
+++ run_tests.py
@@ -1,15 +1,16 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
+import os
 from django.conf import settings
 from django.core.management import call_command
 
 
 settings.configure(
-    INSTALLED_APPS=('autoslug',),
+    INSTALLED_APPS=('autoslug', 'django_coverage'),
     DATABASE_ENGINE='sqlite3',
     AUTOSLUG_SLUGIFY_FUNCTION='django.template.defaultfilters.slugify',
+    COVERAGE_REPORT_HTML_OUTPUT_DIR = os.path.join('.', 'coverage')
 )
 
 if __name__ == "__main__":
-    call_command('test', 'autoslug')
+    call_command('test_coverage', 'autoslug')
