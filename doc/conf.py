af1a9c33075bacd6ced786992e3970ae6b146ba6
Andrey Mikhaylenko <neithere@gmail.com>
2013-07-05 17:30:57 +0600

    Bump year in docs

--- doc/conf.py
+++ doc/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Django Autoslug'
-copyright = u'2009, Andy Mikhailenko'
+copyright = u'2009—2013, Andy Mikhailenko'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
