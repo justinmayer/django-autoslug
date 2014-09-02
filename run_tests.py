33a9f155b35922091cb3a4e8e0cc309b57aada34
Andrey Mikhaylenko <neithere@gmail.com>
2014-09-02 12:47:02 +0600

    Fix #18: tests were not collected on Python 3.x

--- run_tests.py
+++ run_tests.py
@@ -20,13 +20,14 @@ conf = dict(
     AUTOSLUG_SLUGIFY_FUNCTION = 'django.template.defaultfilters.slugify',
 )
 
+# to work with django >= 1.6
+conf.update(TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner')
+
 # django-coverage does not support Python 3 yet
 if sys.version < '3.0':
     conf['INSTALLED_APPS'].append('django_coverage')
     conf.update(COVERAGE_REPORT_HTML_OUTPUT_DIR = os.path.join('.', 'coverage'))
     test_command = 'test_coverage'
-    # to work with django >= 1.6
-    conf.update(TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner')
 else:
     test_command = 'test'
 
