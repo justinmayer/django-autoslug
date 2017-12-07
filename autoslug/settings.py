b92ff34c895b00e219a72986920973a8bbe3b3fb
Chris-Questad <chrisqstd@aol.com>
2017-12-06 19:23:23 -0800

    Added Django 2.0 support

--- autoslug/settings.py
+++ autoslug/settings.py
@@ -58,7 +58,12 @@ Django settings that affect django-autoslug:
 
 """
 from django.conf import settings
-from django.core.urlresolvers import get_callable
+
+# Fixes django.core.urlresolvers import error in Django 2.0
+if django.VERSION[0] >= 2:
+    from django.urls import get_callable
+else:
+    from django.core.urlresolvers import get_callable
 
 # use custom slugifying function if any
 slugify_function_path = getattr(settings, 'AUTOSLUG_SLUGIFY_FUNCTION', 'autoslug.utils.slugify')
