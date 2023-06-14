b5209a24a8615603fa77489d67dacd01beda6981
Justin Mayer <entroP@gmail.com>
2023-06-14 14:17:03 +0200

    Update documentation build configuration

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Django Autoslug'
-copyright = f'2008—{datetime.date.today().year}, Andy Mikhailenko'
+copyright = f'2017—{datetime.date.today().year}, Justin Mayer • 2008—2016, Andy Mikhailenko'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -131,7 +131,7 @@ else:
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
